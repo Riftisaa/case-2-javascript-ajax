ee43f5962d26aace5083edc198eed7e6899ae9e7
Riftisaa <riftisa@student.ub.ac.id>
2024-06-01 20:37:27 +0700

    mvc-mysql

--- model/backRegister.php
+++ model/backRegister.php
@@ -7,7 +7,7 @@ include 'koneksi.php';
     $password = $_POST['password'];
     $sql = "INSERT INTO user (username ,email, password) VALUES ('$username','$email', '$password')";
     if ($conn->query($sql)) {
-        header("Location: login.php");
+        header("Location: ../view/login.php");
     } else {
         echo '<script>alert("Username sudah digunakan oleh akun lain!");</script>';
     }
